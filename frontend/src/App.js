692fec9423cb9364fb12bc28a35003d74ed22e00
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-02-22 10:52:22 +0000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I'm going to build my CV by just using React
         </p>
         <a
           className="App-link"
